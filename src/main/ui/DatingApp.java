f08eb0fcb031d363996f10b9f163b9f87072b09c
Darshan Punjabi <luigi140@students.cs.ubc.ca>
2021-02-14 22:06:16 -0800

    Few Updates

---
+++ src/main/ui/DatingApp.java
@@ -0,0 +1,9 @@
+package ui;
+
+//import ca.ubc.cpsc210.main.model.StudentProfile;
+
+import java.util.Scanner;
+
+public class DatingApp {
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,9 +1,10 @@
 package ui;
 
 
+
 public class Main {
     public static void main(String[] args) {
-
+        new DatingApp();
     }
 
 }
