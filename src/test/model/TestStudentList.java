47a8a559a784f9a23d9ee328c779e96176b0dd4b
Darshan Punjabi <luigi140@students.cs.ubc.ca>
2021-03-04 15:05:56 -0800

    Aiming for higher Code Coverage

--- src/test/model/TestStudentList.java
+++ src/test/model/TestStudentList.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StudentListTest {
+public class TestStudentList {
 
     StudentList testStudentList;
     StudentProfile student1;
